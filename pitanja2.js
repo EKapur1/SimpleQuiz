a52c21049a25d582e941654ad32ec68ea294ed73
Belmin Mujan <mujan.belmin98@gmail.com>
2020-11-18 22:55:08 +0100

    Firste commit

---
+++ pitanja2.js
@@ -0,0 +1,52 @@
+let pitanja=[
+        {
+                  //Unijeti pitanje
+                p:"koliko cores imaju i5 procesori prije 8 generacije?",  
+                o:[
+                        //Unijeti odgovore
+                        "1 core",       //o1   (redni broj tacnog odgovora)                                  
+                        "2 core",       //o2   (redni broj tacnog odgovora)        
+                        "3 core",       //o3   (redni broj tacnog odgovora)        
+                        "4 core",       //o4...
+                        "7 core"       
+                ],
+                to:"o2"                //unijeti redni broj tacnog odgoora                                    
+        },{
+                p:"Koliko cores imaju i5 procesori poslije 8 gen?",
+                o:[
+                        "2 core",
+                         "4 core",
+                         "3 core",
+                         "6 core"
+                ],
+                to:"o2"
+        },{
+                p:"Koji ram se koristi za 3gen intel procesora?",
+                o: [
+                        "ddr1",
+                         "ddr2",
+                         "ddr3",
+                         "ddr4"
+                ],
+                to:"o3"
+        },{
+                p:"koliko rama je dovoljno za normalan rad danasnjice?",
+                o:[
+                        "2gb",
+                         "4gb",
+                         "8gb",
+                         "16gb",
+                ],
+                to:"o3"
+        },{
+                p:"Koje graficke su najbolje za gaming?",
+                o:[
+                        "intel HD",
+                        "ryzen vega",
+                        "nvidia geforce",
+                        "amd gpus",
+                ],
+                to:"o3"
+            }];
+    
+            
---
+++ script.js
@@ -0,0 +1,99 @@
+
+
+
+
+
+let trenutno=0
+// function addPitanje(){
+    //     let p1=pitanja[trenutno];
+    //     document.getElementById("pitanje").innerText=p1.p;
+    //     document.getElementById("aText").innerText=p1.o1;
+    //     document.getElementById("bText").innerText=p1.o2;
+    //     document.getElementById("cText").innerText=p1.o3;
+    //     document.getElementById("dText").innerText=p1.o4;
+    //     uncheck();
+    // }
+    
+{/*          <div class="pitanje">
+                <input class="p" id="a" type="radio" value="o1" name="odgovor">
+                <label class="odg" id="aText"for="a">a</label>
+            </div> */}
+
+    function addPitanje(){
+        let p1=pitanja[trenutno];
+        let prikaz=document.querySelector(".pitanja");
+        prikaz.innerHTML=p1.p;
+        for (let i = 0; i < p1.o.length; i++) {
+            prikaz.innerHTML+=`
+            </br><input class="p" id="a${i}" type="radio" value="o${i+1}" name="odgovor">
+            <label class="odg" id="aText"for="a${i}">${p1.o[i]}</label>
+            `
+        }
+    }
+
+let odgovor=document.getElementsByName('odgovor');
+function uncheck()
+{
+    for(let i=0;i<odgovor.length;i++)
+    {
+        odgovor[i].checked=false;
+    }
+}
+let score=0;
+// function ifChecked()
+// {
+//   for(let i=0;i<odgovor.length;i++)
+//   {
+//       if(odgovor[i].checked)
+//       return true;
+//   }
+//   return false;
+// }
+function provjera(){
+for(let i=0;i<odgovor.length;i++)
+{
+    if(odgovor[i].checked)
+          if(odgovor[i].value==pitanja[trenutno].to)
+          {
+//              alert("tacno");
+                score++;
+          }
+//else alert("netacno");
+}};
+let duzina=pitanja.length-1;
+function displayResult()
+{
+    document.querySelector(".pitanja").style.display="none";
+    document.querySelector("#pitanje").style.display="none";
+    document.querySelector(".rezultat").innerHTML=`<div class="pitanja" style="margin: 50px;">Tacno ste odgovorili ${score} od ${pitanja.length} odgovora.</div>`;
+    document.querySelector(".rezultat").style.display="block";
+    score=0;
+    
+};
+addPitanje();
+document.getElementById("dalje").addEventListener('click', function(){
+    
+            provjera();
+            trenutno++;
+            if(trenutno<pitanja.length)
+            {
+                document.getElementById("dalje").innerText="Dalje";
+                addPitanje();
+                document.querySelector(".pitanja").style.display="block";
+                document.querySelector("#pitanje").style.display="block";
+                document.querySelector(".rezultat").style.display="none";
+        
+            }
+            else 
+            {
+                displayResult();
+                trenutno=0;
+                document.getElementById("dalje").innerText="Ponovo";
+                addPitanje();
+                trenutno--;
+            }
+        
+    })
+
+
+
